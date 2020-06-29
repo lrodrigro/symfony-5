1a36bfad4948ec16f2c0877aef33848824ff967b
Luis Rodriguez <luis.rr.3@gmail.com>
2020-06-29 17:10:28 +0200

    Initial commit

---
+++ src/Controller/WildController.php
@@ -0,0 +1,38 @@
+<?php
+// src/Controller/WildController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+* @Route("/wild", name="wild_")
+*/
+
+class WildController extends AbstractController
+{
+    /**
+    * @Route("/show/{slug}",
+    *     name="show"
+    * )
+    */
+
+    public function show(string $slug = ''): Response
+    {
+        if (empty($slug)) {
+            $slugOut = "Aucune série sélectionnée, veuillez choisir une série";
+        } 
+
+        //$slugConver = str_replace('-', '', $slug); 
+        
+        if (!ctype_lower(str_replace('-', '', $slug)) || strpos($slug, '_')) {
+            throw $this->createNotFoundException();
+        } else {
+            $slugOut = ucwords(str_replace('-', ' ', $slug));  
+        }
+
+        return $this->render('wild/show.html.twig', ['slug' => $slugOut]);
+    }
+}
